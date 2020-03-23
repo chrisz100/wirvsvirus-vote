3744e5fb585dce6ed84731c0a9dcfe6c67b902d2
Christian Jantz <chris@level25.de>
2020-03-23 20:54:49 +0100

    update

--- generate-playlists/item_all.py
+++ generate-playlists/item_all.py
@@ -31,10 +31,12 @@ mapping_file = os.environ["VIDEO_MAPPING"]
 
 playlist_prefix = '#wirvsvirushack Alle Projekte #'
 
-def get_authenticated_service():
+playlists_all_videos = []
+
+def get_authenticated_service(secrets_file):
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes
+        secrets_file, scopes
     )
     credentials = flow.run_console()
     youtube = build(api_service_name, api_version, credentials=credentials)
@@ -42,67 +44,68 @@ def get_authenticated_service():
 
 
 def run_insert(args):
-    client, position, backoff = args
+    client, playlist = args
 
-    # Create new playlist
-    request = client.playlists().insert(
-        part="snippet,status",
-        body={
-          "snippet": {
-            "title": f"{playlist_prefix}{position}",
-            "description": f"{playlist_prefix}{position}",
-            "tags": [
-              "#WeVsVirus",
-              "Hackathon"
-            ],
-            "defaultLanguage": "de"
-          },
-          "status": {
-            "privacyStatus": "unlisted"
-          }
-        }
-    )
-    try:
-        playlist = request.execute()
-    except BaseException as e:
-        logging.error('Failed creating playlist: %s', e)
-        backoff_time = 1.5**backoff * 60
-        logging.info('Sleeping for %ss', backoff_time)
-        sleep(backoff_time)
-        return run_insert((client, position, backoff + 1))
-    else:
-        logging.info('Created playlist: %s', playlist)
+    # # Create new playlist
+    # request = client_playlist.playlists().insert(
+    #     part="snippet,status",
+    #     body={
+    #       "snippet": {
+    #         "title": f"{playlist_prefix}{position}",
+    #         "description": f"{playlist_prefix}{position}",
+    #         "tags": [
+    #           "#WeVsVirus",
+    #           "Hackathon"
+    #         ],
+    #         "defaultLanguage": "de"
+    #       },
+    #       "status": {
+    #         "privacyStatus": "unlisted"
+    #       }
+    #     }
+    # )
+    # try:
+    #     playlist = request.execute()
+    # except BaseException as e:
+    #     logging.error('Failed creating playlist: %s', e)
+    #     backoff_time = 1.5**backoff * 60
+    #     logging.info('Sleeping for %ss', backoff_time)
+    #     sleep(backoff_time)
+    #     return run_insert((client_playlist, client, position, backoff + 1))
+    # else:
+    #     logging.info('Created playlist: %s', playlist)
 
-        # First, we read the values from a file into our dictionary
-        with open(mapping_file) as f:
-            video_array = [line.rstrip("\n") for line in f]
-        shuffle(video_array)
+    # First, we read the values from a file into our dictionary
+    with open(mapping_file) as f:
+        video_array = [line.rstrip("\n") for line in f]
+    shuffle(video_array)
 
-        # Then loop over the dictionary and add videos to playlists
-        for video_id in video_array:
-            try:
-                response = client.playlistItems().insert(
-                    part="snippet",
-                    body={
-                        "snippet": {
-                            "playlistId": playlist['id'],
-                            "position": 0,
-                            "resourceId": {"kind": "youtube#video", "videoId": video_id},
-                        },
+    # Then loop over the dictionary and add videos to playlists
+    for video_id in video_array:
+        try:
+            response = client.playlistItems().insert(
+                part="snippet",
+                body={
+                    "snippet": {
+                        "playlistId": playlist,
+                        "position": 0,
+                        "resourceId": {"kind": "youtube#video", "videoId": video_id},
                     },
-                ).execute()
-                logging.info('Inserted video into playlist %s: %s', playlist['id'], response)
-            except:
-                logging.error('Inserting video failed: %s', video_id)
+                },
+            ).execute()
+            logging.info('Inserted video into playlist %s: %s', playlist['id'], response)
+        except:
+            logging.error('Inserting video failed: %s', video_id)
 
-        return playlist
+    return playlist
 
 
 def main():
-    client = get_authenticated_service()
+    print('_________ Video Client _________')
+    client_video = get_authenticated_service(client_secrets_file)
 
     with ProcessPoolExecutor() as executor:
-        playlists = executor.map(run_insert, zip(repeat(client), range(5), repeat(1)))
+        playlists = executor.map(run_insert, zip(repeat(client_video), playlists_all_videos))
 
     playlist_ids = [playlist['id'] for playlist in playlists]
     logging.info('Done! playlists: %s', playlist_ids)
